f137a0722e9ae2306a50e5f22ce9a0990a7c0583
Crowdbotics <team@crowdbotics.com>
2021-09-07 16:12:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "testfigaap_dev_22887Identifier",
+  url: "https://testfigaap_dev_22887Identifier.botics.co",
+  api: "https://testfigaap_dev_22887Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
